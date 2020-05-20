ba7dc0d7791155e0915ab0143dd3140e64331515
Vytaute <vytaute.lipeikaite@gmail.com>
2020-05-20 19:36:51 +0300

    pirmieji pokyciai

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace projektasGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sveiki visi :) ");
         }
     }
 }
