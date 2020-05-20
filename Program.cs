a396b5b7a1816bc8bb611de803a48f4c825fe6b5
Vytaute <vytaute.lipeikaite@gmail.com>
2020-05-20 20:43:08 +0300

    add sum

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@ namespace projektasGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Sveiki visi :) ");
-            Console.WriteLine("Sveiki visi :) ");
-            Console.WriteLine("Sveiki visi :) ");
-            Console.WriteLine("Sveiki visi :) ");
+
+            int x = 2;
+            int y = 3;
+            int sum = x + y;
+            Console.WriteLine("sum: {0}", sum);
         }
     }
 }
