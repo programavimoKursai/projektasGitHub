6d69967e1201b46b52f5fafa99f9c2bb8cfd9435
programavimoKursai <65595102+programavimoKursai@users.noreply.github.com>
2020-05-20 21:01:47 +0300

    remove some parts

--- Program.cs
+++ Program.cs
@@ -6,12 +6,7 @@ namespace projektasGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sveiki visi :) ");
-
             int x = 2;
-            int y = 3;
-            int sum = x + y;
-            Console.WriteLine("sum: {0}", sum);
         }
     }
 }
